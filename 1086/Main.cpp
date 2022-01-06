a2d9772b4bbd6187015ff795ba08d306ff30b18c
Jimi <qkrwoals1588@gmail.com>
2022-01-06 22:43:46 +0900

    "1086"

---
+++ 1086/Main.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main(void) {
+
+	int w, h, b;
+	double output;
+	scanf("%d %d %d", &w, &h, &b);
+
+	if (w >= 1 && h >= 1 && w <= 1024 && h <= 1024&&b%4==0) {
+
+		output = ((double)(w * h * b)) / (8*1024*1024);
+	}
+	printf("%.2f", output);
+	return 0;
+}
